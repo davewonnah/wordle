8c42c28ba1158738fc74858dba58cbfc95bb4903
Dave Wonnah <wonnahdave@gmail.com>
2023-05-04 16:33:20 +0000

    started adding letters to the boxes when a key is click

--- apps.js
+++ apps.js
@@ -1,24 +1,26 @@
-const keys = document.querySelectorAll('.key');
-const boxes = document.querySelectorAll('.box');
-
-
-
-
 const words = ["apple", "table", "house", "knife", "dream"];
 
 const randomIndex = Math.floor(Math.random() * words.length);
-
-// Get the random word
 const randomWord = words[randomIndex];
-
 console.log(randomWord);
 
+// Function to add key text content to boxes starting from the first box
+function addKey() {
+    const keys = document.querySelectorAll('.key');
+    const boxes = document.querySelectorAll('.box');
+    let currentIndex = 0;
+  
+    keys.forEach((key) => {
+      key.addEventListener('click', () => {
+        if (currentIndex < boxes.length) {
+          boxes[currentIndex].textContent = key.textContent;
+          currentIndex++;
+        }
+      });
+    });
+  }
+  
+  // Call the function to add key text content to boxes starting from the first box
+  addKey();
 
-keys.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    const buttonText = button.textContent;
-    boxes[index].textContent = buttonText;
-  });
-});
 
-console.log(boxes[0]);
